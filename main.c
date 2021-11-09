452fb6689ac65357187a31bed7de44fb046f3862
akshata112 <93388822+akshata112@users.noreply.github.com>
2021-11-09 22:24:58 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,70 +1,33 @@
 #include <stdio.h>
-
-int main(int argc, char **argv)
+#include <conio.h>
+#define SIZE 10
+int ar[SIZE],top=-1;
+void push(int value)
 {
-	float a,b,result;
-    int i=0,reminder;
-    char op;
-    while(i<10){
-    printf("\nenter a operator b:");
-    scanf("%f%c%f",&a,&op,&b);
-    switch(op){
-        case'+':
-        result=a+b;
-        printf("result is %f",result);
-        break;
-        case'-':
-        result=a-b;
-        printf("result is %f",result);
-        break;
-        case'*':
-        result=a*b;
-        printf("result is %f",result);
-        break;
-        case'/':
-        result=a/b;
-        printf("result is %f",result);
-        break;
-        case'<':{
-        if(a<b){
-        printf("a is less than b");}
-        else{
-        printf("b is lesser than a");}}
-        break;
-        case'>':{
-        if(a>b){
-        printf("a is greater than b");}
-        else{
-        printf("b is greater than a");}}
-        break;
-        case'=':{
-        if(a==b){
-           printf("a is equal to b"); 
-        }
-        else{
-           printf("a is not equal to b");
-        }}
-        break;
-        case'%':
-        reminder=(int)a%(int)b;
-        printf("reminder is %d",reminder);
-        break;
-        case'!':
-        if(a!=b){
-            printf("a is not equal to b");
-        }
-        else{
-            printf("a and b are equal");
-        }
-        break;
-        //i is for incrementing
-        case'i':
-        result=a+1;
-        printf("result is %f",result);
-        break;
-        default:
-        printf("invalid operator");
+    if(top==10){
+        printf("ínvalid");
     }
-    i++;}
-	return 0;
+        else{
+            ar[top++]=value;
+            printf("\n ar[%d] is %d",top,value);
+        }
+}
+void pop()
+{
+    if(top==-1){
+        printf("stack is empty");
+    }
+        else{
+            printf("\n%d",ar[--top]);
+        }
+}
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    pop();
+    pop();
+    return 0;
 }
