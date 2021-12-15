3f3ffe13cbd441ef0e654de7bf5e83e3e90f6001
akshata112 <93388822+akshata112@users.noreply.github.com>
2021-12-15 19:45:51 +0530

    Update quad.java

--- quad.java
+++ quad.java
@@ -1,40 +1,38 @@
 import java.util.*;
-import java.lang.*;
-class quad{
-public static void main(String args[])
+class Main
 {
-double a,b,c,r1=0,r2=0,d;
-Scanner in=new Scanner(System.in);
+  public static void main (String args[])
+  {
+    double a, b, c, r1 = 0, r2 = 0, d;
+    Scanner in = new Scanner (System.in);
+      System.out.println ("Enter coefficients a b c of quadratic equation");
+      System.out.print ("a=");
+      a = in.nextDouble ();
+      System.out.print ("b=");
+      b = in.nextDouble ();
+      System.out.print ("c=");
+      c = in.nextDouble ();
 
-System.out.println("enter a");
-a=in.nextDouble();
-System.out.println("enter b");
-b=in.nextDouble();
-System.out.println("enter c");
-c=in.nextDouble();
-
-d=b*b-4*a*c;
-if(d<0)
-{
-System.out.println("no real solution");
+      d = b * b - 4 * a * c;
+    if (d < 0)
+      {
+	System.out.println ("No real solution");
+      }
+    else if (d > 0)
+      {
+	r1 = (-b + Math.sqrt (d)) / (2 * a);
+	r2 = (-b - Math.sqrt (d)) / (2 * a);
+	
+	System.out.println ("root1 is " + r1);
+	System.out.println ("root2 is " + r2);
+      }
+    else if (d == 0)
+      {
+	r1 = (-b / (2 * a));
+	r2 = (-b / (2 * a));
+	System.out.println("Equal roots");
+	System.out.println ("root1 is " + r1);
+	System.out.println ("root2 is " + r2);
+      }
+  }
 }
-else if (d>0)
-{
-r1=(-b+Math.sqrt(d))/(2*a);
-r2=(-b-Math.sqrt(d))/(2*a);
-System.out.println("root1 is"+r1);
-System.out.println("root2 is"+r2);
-}
-else if(d==0)
-{
-r1=(-b/(2*a));
-r2=(-b/(2*a));
-System.out.println("root1 is"+r1);
-System.out.println("root2 is"+r2);
-}
-}
-}
-
-
-
-
